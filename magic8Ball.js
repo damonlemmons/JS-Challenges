1c6cd97b7ed4782baad189217332a6be515bc906
learn <learn@learn07-2.localdomain>
2018-03-30 11:40:16 -0700

    Adds magic 8ball

---
+++ magic8Ball.js
@@ -0,0 +1,11 @@
+var answers = ["Yup!", "Fuhgeddaboudit", "Maybe", "Ask: what would your mother do?", "Ask: what would an Australian do", "then do the opposite", "Your answer here"]
+
+function eightBall() {
+  question = prompt("What would you like to know human?")
+  while (question !== "stop") {
+    alert("The answer to " + question + " is: " + answers[Math.floor(Math.random() * answers.length)])
+    question = prompt("What else would you like to know?")
+  }
+  }
+
+eightBall()
---
+++ passwordChecker.js
@@ -0,0 +1,31 @@
+# JS-Challenges
+function checker(name,password) {
+valid = false
+if (name == password) {
+return "ID and password cannot be the same"
+} else if (name.length < 6 || password.length < 6) {
+return "ID and password have to be at least six characters long" 
+} else if (name.includes("!") || (name.includes("#") || (name.includes("$")) === true)) {
+return "ID cannot contain the following characters: ! # $"
+} else if (password.includes("!") || (password.includes("#") || (password.includes("$"))) !== true) {
+return "Password has to contain at least one of: ! # $"
+} else if (password.includes("password")) {
+return "Password cannot include password"
+} else {
+valid = true
+return valid
+}
+}
+
+function createUser() {
+var name = prompt("Enter Username")
+var password = prompt("Enter a Password")  
+if (checker(name,password) === true) {
+return "Welcome to the Club " + name
+} else {
+return "FAIL!:  " + checker(name,password)
+}
+}
+
+
+createUser()
